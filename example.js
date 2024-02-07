f56b99fc1b047a5cbf3312a2e608e0019a68a96d
Krhuhun <72083150+jorgelisboa@users.noreply.github.com>
2024-02-07 11:52:17 -0300

    Create example.js

---
+++ example.js
@@ -0,0 +1,10 @@
+// Variável da pergunta analisada
+const question = 'qual time você torce?"
+
+// Verifica se é pergunta
+
+// Supõe o que é a pergunta
+
+// Trata a resposta
+
+// Monta a pergunta
