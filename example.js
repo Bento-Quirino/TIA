ee879f81eb55386a8f749bbc96d9454da2108646
Krhuhun <72083150+jorgelisboa@users.noreply.github.com>
2024-02-07 12:18:20 -0300

    Update example.js
    - toUpperCase
    - toLowerCase
    - Split
    - console.log()

--- example.js
+++ example.js
@@ -1,5 +1,14 @@
 // Variável da pergunta analisada
-const question = 'qual time você torce?"
+const question = 'quaL_tiMe_vOcê_torce?"
+
+// toUppweCase, toLowerCase
+console.log(question.toUpperCase()) // Deixa tudo em CAIXA ALTA
+console.log(question.toLowerCase()) // Deixa tudo em minusculo
+// Mostra no terminal
+console.log(question) // Printa no terminal
+
+// Split
+console.log(question.split("_")) // Separa onde tem "_"
 
 // Verifica se é pergunta
 
