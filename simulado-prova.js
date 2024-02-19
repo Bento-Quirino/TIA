2155a5db8503c1cea7092cd3814c7b5f38278ef7
krhuhun <jorgelisboa659@gmail.com>
2024-02-19 17:27:17 -0300

    Simulado como fazer um chatbot

---
+++ simulado-prova.js
@@ -0,0 +1,18 @@
+// Simulado para a prova de TIA
+
+// Crie um "chatbot" que responda perguntas sobre o curso de Sistemas de Informação no site da PUC.
+// O chatbot deve seguir os seguintes passos:
+// 1. Mandar uma mensagem de boas vindas aleatória, sendo elas as seguintes:
+//    - "Olá, me chamo Sabini, como posso te ajudar?"
+//    - "Oi, sou a Sabini, a ajudante virtual da PUC, com o que precisa de ajuda?"
+//    - "E aí, tudo bem? Eu sou a Sabini, estou aqui para te ajudar!"
+// 2. Receber a pergunta do usuário
+// 3. Sabini não responde perguntas que não sejam sobre o curso de Sistemas de Informação, então ela deve verificar se a pergunta é sobre o curso.
+// Os temas que podemos perguntar são:
+//    - Qual a mensalidade do curso de Sistemas de Informação, tem bolsa, se sim quanto?
+//    - Quais as matérias do curso de Sistemas de Informação ou o que se estuda no curso de Sistemas de Informação
+//    - Intercâmbio no curso de Sistemas de Informação ou intercâmbio na PUC, como funciona?
+//    - Estágio no curso de Sistemas de Informação, como funciona?
+// 4. Se a pergunta não for sobre esses temas, Sabini deve responder que não pode ajudar com essa pergunta.
+// 5. Se a pergunta não for sobre a faculdade, deve responder que as perguntas devem ser sobre o curso de Sistemas de Informação.
+// 6. Será disponibilizado um arquivo com parte do código feito, continue seguindo a lógica e use o que foi dado.
