fbd44c448298c166e8aff35c0e75785a43507666
krhuhun <jorgelisboa659@gmail.com>
2024-02-27 16:26:08 -0300

    simulado chatbot

---
+++ Simulados/chatbot/can-i-answer.js
@@ -0,0 +1,38 @@
+// O que é futebol?
+export const palavrasFutebol = [
+    "o", "que", "é", "futebol",
+    "o", "q", "é", "futebol",
+    "q", "é", "futebol",
+    "o", "que", "e", "futebol",
+    "o", "q", "e", "futebol",
+    "me", "explica", "o", "que", "é", "o", "futebol",
+    "sabe", "me", "dizer", "o", "que", "é", "futebol"
+]
+
+// Quais as regras do futebol?
+export const palavrasRegras = [
+    "quais", "as", "regras", "do", "futebol",
+    "quais", "sao", "as", "regras", "do", "futebol",
+    "regras", "do", "futebol",
+    "futebol", "regras",
+    "como", "funciona", "o", "futebol"
+]
+
+// Que time ele mais gosta?
+export const palavrasTimeGosta = [
+    "que", "time", "ele", "mais", "gosta",
+    "qual", "time", "você", "torce",
+    "time", "preferido",
+    "clube", "do", "coração",
+    "time", "do", "coração",
+    "que", "time", "você", "torce"
+]
+
+// Palmeiras tem mundial?
+export const palavrasMundialPalmeiras = [
+    "palmeiras", "tem", "mundial",
+    "mundial", "do", "palmeiras",
+    "palmeiras", "mundial",
+    "palmeiras", "tem", "copa", "do", "mundo",
+    "palmeiras", "campeão", "mundial"
+]
---
+++ Simulados/chatbot/chat-bot-v1.js
@@ -0,0 +1,12 @@
+// O que ele vai poder responder?
+// - O que é futebol?
+// - Quais as regras do futebol?
+// - Que time ele mais gosta?
+// - Palmeiras tem mundial?
+
+const resposta = {
+    whatIsSoccer: 0,
+    soccerRules: 0,
+    favoriteTeam: 0,
+    palmeirasMundial: 0,
+}
