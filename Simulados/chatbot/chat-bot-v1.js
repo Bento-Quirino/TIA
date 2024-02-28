499c36a1af48bd27fc1f09ad1854f6a371cf7cb5
krhuhun <jorgelisboa659@gmail.com>
2024-02-28 11:09:10 -0300

    IA E Chatbot

--- Simulados/chatbot/chat-bot-v1.js
+++ Simulados/chatbot/chat-bot-v1.js
@@ -1,12 +1,14 @@
+import { palavrasFutebol, palavrasMundialPalmeiras, palavrasRegras, palavrasTimeGosta, mostProbablyQuestion } from './can-i-answer.js'
 // O que ele vai poder responder?
 // - O que é futebol?
 // - Quais as regras do futebol?
 // - Que time ele mais gosta?
 // - Palmeiras tem mundial?
 
-const resposta = {
-    whatIsSoccer: 0,
-    soccerRules: 0,
-    favoriteTeam: 0,
-    palmeirasMundial: 0,
-}
+console.log(palavrasFutebol)
+
+// TENSIONAR ALGUM DOS PONTOS
+palavrasFutebol.matched = 4
+
+// Verfica quem foi mais tensionado
+console.log(mostProbablyQuestion())
