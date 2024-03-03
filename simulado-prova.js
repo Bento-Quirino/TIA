69305600c874bcd218798343599d63b8b831f7a6
Krhuhun <72083150+jorgelisboa@users.noreply.github.com>
2024-03-02 21:00:05 -0300

    Update simulado-prova.js

--- simulado-prova.js
+++ simulado-prova.js
@@ -1,18 +1,17 @@
 // Simulado para a prova de TIA
 
-// Crie um "chatbot" que responda perguntas sobre o curso de Sistemas de Informação no site da PUC.
+// Crie um "chatbot" que responda perguntas sobre o curso de Informática no site do Bento Quitino.
 // O chatbot deve seguir os seguintes passos:
 // 1. Mandar uma mensagem de boas vindas aleatória, sendo elas as seguintes:
-//    - "Olá, me chamo Sabini, como posso te ajudar?"
-//    - "Oi, sou a Sabini, a ajudante virtual da PUC, com o que precisa de ajuda?"
-//    - "E aí, tudo bem? Eu sou a Sabini, estou aqui para te ajudar!"
+//    - "Olá, me chamo Bento, como posso te ajudar?"
+//    - "Oi, sou o Bento, o ajudante virtual do Bento Quirino, com o que precisa de ajuda?"
+//    - "E aí, tudo bem? Eu sou o Bento, estou aqui para te ajudar!"
 // 2. Receber a pergunta do usuário
-// 3. Sabini não responde perguntas que não sejam sobre o curso de Sistemas de Informação, então ela deve verificar se a pergunta é sobre o curso.
+// 3. Bento não responde perguntas que não sejam sobre o curso de Informática, então ele deve verificar se a pergunta é sobre o curso.
 // Os temas que podemos perguntar são:
-//    - Qual a mensalidade do curso de Sistemas de Informação, tem bolsa, se sim quanto?
-//    - Quais as matérias do curso de Sistemas de Informação ou o que se estuda no curso de Sistemas de Informação
-//    - Intercâmbio no curso de Sistemas de Informação ou intercâmbio na PUC, como funciona?
-//    - Estágio no curso de Sistemas de Informação, como funciona?
-// 4. Se a pergunta não for sobre esses temas, Sabini deve responder que não pode ajudar com essa pergunta.
-// 5. Se a pergunta não for sobre a faculdade, deve responder que as perguntas devem ser sobre o curso de Sistemas de Informação.
+//    - Qual a mensalidade do curso, tem bolsa, se sim quanto?
+//    - Quais as matérias do curso ou o que se estuda no curso.
+//    - Estágio no curso, como funciona?
+// 4. Se a pergunta não for sobre esses temas, Bento deve responder que não pode ajudar com essa pergunta.
+// 5. Se a pergunta não for sobre a instituição, deve responder que as perguntas devem ser sobre o curso de Informática.
 // 6. Será disponibilizado um arquivo com parte do código feito, continue seguindo a lógica e use o que foi dado.
